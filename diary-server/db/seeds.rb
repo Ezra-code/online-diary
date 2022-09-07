cd3a2be4d92bcc5a609e8bbf8ba524acccdac633
Ezra-code <njerukithinji872@gmail.com>
2022-09-07 14:33:23 +0300

    add other emotion files in emotions db

--- diary-server/db/seeds.rb
+++ diary-server/db/seeds.rb
@@ -2,8 +2,11 @@ puts "🌱 Seeding spices..."
 justice = Event.create(body: "this is when justice was served before everything else was done or made", emotion_id: 2)
 killer = Event.create(body: "the killer was found and bound. he was wound in a round chain", emotion_id: 1)
 
-happy = Emotion.create(name: "happy")
-shock = Emotion.create(name: "shocked")
+happy = Emotion.create(name: "Happy")
+shock = Emotion.create(name: "Shocked")
+sad = Emotion.create(name: "Sad")
+angry = Emotion.create(name: "Angry")
+
 # Seed your database here
 
 puts "✅ Done seeding!"
