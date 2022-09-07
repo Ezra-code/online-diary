6ab4c1d2d0bcfe8d34ff5104d865a8870ec030c2
Ezra-code <njerukithinji872@gmail.com>
2022-09-07 15:42:30 +0300

    this will fetch data from the database to our react

--- diary/src/components/Home.js
+++ diary/src/components/Home.js
@@ -1,4 +1,15 @@
+import { useEffect, useState } from "react";
+
+
 const Home = () => {
+    let [emote, setEmote] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:9292/emotions")
+            .then(r => r.json())
+        .then(data => console.log(data))
+    }, [])
+
     return (<p>this is the hompage</p>);
 }
  
