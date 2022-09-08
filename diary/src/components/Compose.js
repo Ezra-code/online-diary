ddaf86cb88b43f9f63c3d45d3d36b8f5ddc2ca75
Ezra-code <njerukithinji872@gmail.com>
2022-09-08 15:33:56 +0300

    create a selction of buttons to choose from

--- diary/src/components/Compose.js
+++ diary/src/components/Compose.js
@@ -1,5 +1,25 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
 const Compose = () => {
-    return ( <h1>Write about your day</h1> );
-}
- 
+  const [note, setNote] = useState({
+    emotion: "",
+    details: "",
+  });
+  let emotions = ["Happy", "Sad", "Angry", "Calm", "Lazy", "Energetic"];
+
+  let radr = emotions.map((element) => (
+    <button className="emote">{element}</button>
+  ));
+
+  return (
+    <div className="compose">
+      <h1>Write About your Day</h1>
+      <h2>How do you feel abut your day</h2>
+
+      <div className="combine">{radr}</div>
+    </div>
+  );
+};
+
 export default Compose;
