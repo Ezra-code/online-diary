53569500f8d39eacad6d85b1478c93ed2cf40d08
Ezra-code <njerukithinji872@gmail.com>
2022-09-10 22:37:38 +0300

    Add readme

--- diary/src/components/Eventlist.js
+++ diary/src/components/Eventlist.js
@@ -4,6 +4,11 @@ import { useParams } from "react-router-dom";
 const Events = () => {
   const [notes, setNotes] = useState([]);
   const { id } = useParams();
+  const [show, setShow] = useState(false);
+  let [change, setChange] = useState({
+    title: "",
+    body: ""
+  })
 
   useEffect(() => {
     fetch(`http://localhost:9292/emotions/${id}`)
@@ -17,21 +22,33 @@ const Events = () => {
   function handleDelete(ele) {
     console.log(ele.id);
   }
+//handle the form submission for edit
+  function handleSubmit() {
+    
+  }
 
   function handleCopy(param) {
-    navigator.clipboard.writeText(param.body)
+    navigator.clipboard.writeText(param.body);
+  }
+
+  function handleEdit(element) {
+    setShow(!show);
+    console.log(`edit:${element.body}`, element.title);
   }
 
   function handleSound(param) {
-    let utterance = new SpeechSynthesisUtterance(`${param.title}, ${param.body}`);
-    speechSynthesis.speak(utterance)
-    console.log(param.id)
+    let utterance = new SpeechSynthesisUtterance(
+      `${param.title}, ${param.body}`
+    );
+    speechSynthesis.speak(utterance);
+    console.log(param.id);
   }
 
   const eve = notes.map((element) => {
     let uploadDate = element.created_at;
     let newDate = uploadDate.split("T");
     return (
+      //display the title and creation date for element from events table
       <div className="story-prev" key={element.id}>
         <div className="nav">
           <h1>{element.title}</h1>
@@ -49,16 +66,35 @@ const Events = () => {
           <p>{element.body}</p>
           <div className="features">
             <ul>
-              <li className="sound" onClick={() => { handleSound(element) }}>
+              <li
+                className="sound"
+                onClick={() => {
+                  handleSound(element);
+                }}
+              >
                 <i className="fa-solid fa-volume-high"></i>
               </li>
-              <li className="copy" onClick={() => { handleCopy(element) }}>
+              <li
+                className="copy"
+                onClick={() => {
+                  handleCopy(element);
+                }}
+              >
                 <i className="fa-solid fa-copy"></i>
               </li>
             </ul>
           </div>
-        </div>
 
+          {/* allow editing of data create form */}
+          {show ? (
+            <form onSubmit={handleSubmit}>
+              <input type="text" name="title" value={element.title} id="change" onChange={(e)=>{setChange(e.target.value)}}/>
+              <textarea name="body" id="" cols="30" rows="10" value={element.body} onChange={(e)=>{setChange(e.target.value)}} />
+              <input type="submit"  value="Save change"/>
+            </form>
+          ) : null}
+        </div>
+        
         <div className="btn">
           <button
             onClick={() => {
@@ -69,12 +105,20 @@ const Events = () => {
             {" "}
             Delete{" "}
           </button>
-          <button className="edit">Edit</button>
+          <button
+            className="edit"
+            onClick={() => {
+              handleEdit(element);
+            }}
+          >
+            {" "}
+            Edit{" "}
+          </button>
         </div>
       </div>
     );
   });
-  console.log(eve);
+
   return <div className="prev">{eve}</div>;
 };
 
---
+++ readme.md
@@ -0,0 +1,49 @@
+# online diary
+ ## By Ezra Njeru
+## Screenshot
+ ![image](./Screenshot.png)
+ ## Table of Content
+ - [Description](#description)
+ - [Features](#features)
+ - [Requirements](#requirements)
+ - [Installation Process](#installation-Process)
+ - [Live Link](#Live-Link)
+ - [Technology  Used](#technology-Used)
+ - [Licence](#licence)
+ - [Authors Info](#Authors-Info)
+ ## Description
+ <p>This is a custom diary where a user is able to see all events that happened in their lives. each event is groupe according to the users feelings or mood that day</p>
+
+## Features
+
+As a user of the application,you will be able to :
+* Select a feeling/mood and see a list of events associated with such
+* see a list of feelings.
+* upload a new event to the list
+[Go Back to the top](#online-diary)
+ ###  Requirements
+ * Access to  a computer or any other gadget
+ * Access to internet
+ ### Installation Process
+ ****
+* Clone to the repo : https://github.com/Ezra-code/online-diary.git
+* Unzip the downloaded files in a folder of choice.
+* Open the index file from the zipped file with any browser.
+ ****
+ [Go Back to the top](#online-diary)
+### Live Link
+
+### Technology  Used
+* JSX - which was used to develope the structure off the pages.
+* CSS - which was used to style the User Interface.
+* REACT - was used to make the page responsive and allow web uploads using the apis
+* Ruby - to create a link between front end and database
+* sqlite - to save our data in order to retrieve it afterwards 
+[Go Back to the top](#online-diary)
+## Instructions
+* The server used to host the backend of the website takes sometime to load so be patient.
+## Licence
+This project is licensed under the terms of the MIT license
+[Go Back to the top](#online-diary)
+## Authors Info
+Ezra Njeru Kithinji 
